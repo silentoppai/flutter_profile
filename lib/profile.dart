c8735d9cf688bc3f1d484b91cc4484b010ccaa77
Your Name <you@example.com>
2020-12-03 15:35:05 +0700

    viewhuador

--- lib/profile.dart
+++ lib/profile.dart
@@ -153,7 +153,7 @@ class MyProfile extends StatelessWidget {
                     leading: Icon(Icons.arrow_forward),
                     title: Align(
                       child: new Text(
-                        'อาจารประจำสาขาวิชาวิทยาการคอมพิวเตอร์,\n\nคณะศิลปศาสตร์และวิทยาศาสตร์,\n\nมหาวิทยาลัยราชภัฏศรีสะเกษ',
+                        'นักศึกษา,\n\nคณะศิลปศาสตร์และวิทยาศาสตร์,\n\nมหาวิทยาลัยราชภัฏศรีสะเกษ',
                         style: contentStyle,
                       ),
                       alignment: Alignment(-1.8, 0),
@@ -206,6 +206,204 @@ class MyProfile extends StatelessWidget {
                     title: Align(
                       child: new Text(
                         'เล่นเปตอง',
+                        style: contentStyle,
+                      ),
+                      alignment: Alignment(-1, 0),
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ListTile(
+                    leading: Icon(Icons.developer_board),
+                    title: Align(
+                      child: new Text(
+                        'แข่งกินเร็ว',
+                        style: contentStyle,
+                      ),
+                      alignment: Alignment(-1, 0),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+              child: Column(
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Center(
+                      child: Text(
+                        "นายกฤติพงษ์ มงคล",
+                        style: bulletStyle,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(50.0),
+                    child: Image.asset(
+                      'assets/imges/viewshowkauy.jpg',
+                      height: 180.0,
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Align(
+                      child: new Text(
+                        'สาขาวิชาวิทยาการคอมพิวเตอร์\nคณะศิลปศาสตร์และวิทยาศาสตร์',
+                        style: contentStyle,
+                      ),
+                      alignment: Alignment(-1, 0),
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "ประวัติการศึกษา",
+                      style: contentBulletStyle,
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.arrow_forward),
+                    title: Align(
+                      child: new Text(
+                        'โรงเรียนนิคมวิทยา',
+                        style: contentStyle,
+                      ),
+                      alignment: Alignment(-1.8, 0),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.arrow_forward),
+                    title: Align(
+                      child: new Text(
+                        'มหาวิทยาลัยราชภัฎศรีสะเกษ',
+                        style: contentStyle,
+                      ),
+                      alignment: Alignment(-1.8, 0),
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "สำหรับติดต่อ",
+                      style: contentBulletStyle,
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.phone_android),
+                    title: Align(
+                      child: new Text(
+                        'ww_ww25401@hotmail.com',
+                        style: contentStyle,
+                      ),
+                      alignment: Alignment(-1, 0),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Align(
+                      child: new Text(
+                        'www.facebook.com/viewkittipong',
+                        style: contentStyle,
+                      ),
+                      alignment: Alignment(-1, 0),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.web),
+                    title: Align(
+                      child: new Text(
+                        'www.comsci-sskru.com',
+                        style: contentStyle,
+                      ),
+                      alignment: Alignment(-1, 0),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.email),
+                    title: Align(
+                      child: new Text(
+                        'kittipong.mong61@sskru.ac.th',
+                        style: contentStyle,
+                      ),
+                      alignment: Alignment(-1, 0),
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "ประวัติการทำงาน",
+                      style: contentBulletStyle,
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.arrow_forward),
+                    title: Align(
+                      child: new Text(
+                        'นักศึกษา',
+                        style: contentStyle,
+                      ),
+                      alignment: Alignment(-1.8, 0),
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "ความสามารถพิเศษ",
+                      style: contentBulletStyle,
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ListTile(
+                    leading: Icon(Icons.developer_board),
+                    title: Align(
+                      child: new Text(
+                        'นักกีฬาฟุตบอล',
+                        style: contentStyle,
+                      ),
+                      alignment: Alignment(-1, 0),
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ListTile(
+                    leading: Icon(Icons.developer_board),
+                    title: Align(
+                      child: new Text(
+                        'นักกีฬาเซปักตะกร้อ',
+                        style: contentStyle,
+                      ),
+                      alignment: Alignment(-1, 0),
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ListTile(
+                    leading: Icon(Icons.developer_board),
+                    title: Align(
+                      child: new Text(
+                        'นักกีฬาเปตอง',
+                        style: contentStyle,
+                      ),
+                      alignment: Alignment(-1, 0),
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ListTile(
+                    leading: Icon(Icons.developer_board),
+                    title: Align(
+                      child: new Text(
+                        'นักกีฬาว่ายน้ำ',
                         style: contentStyle,
                       ),
                       alignment: Alignment(-1, 0),
