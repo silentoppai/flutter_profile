b8afcb57c6d979e51e0582689745dc26d28eefdd
Your Name <you@example.com>
2020-12-03 13:40:48 +0700

    tonnakub

---
+++ lib/page2.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyProfile extends StatelessWidget {
+  MyProfile({Key key}) : super(key: key);
+
+  var sizeBox = 14.0;
+
+  TextStyle bulletStyle = GoogleFonts.prompt(
+    fontSize: 28,
+    fontWeight: FontWeight.bold,
+  );
+
+  TextStyle contentBulletStyle = GoogleFonts.prompt(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+
+  TextStyle contentStyle = GoogleFonts.kanit(
+    fontSize: 18,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Our Pictures',
+          style: contentStyle,
+        ),
+        automaticallyImplyLeading: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+              child: Column(
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Center(
+                      child: Text(
+                        "นายเกรียงศักดิ์ อัครบุตร",
+                        style: bulletStyle,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(50.0),
+                    child: Image.asset(
+                      'assets/imges/tonprofile.jpg',
+                      height: 380.0,
+                    ),
+                  ),
+                  
+                  SizedBox(height: sizeBox),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(50.0),
+                    child: Image.asset(
+                      'assets/imges/prang.jpg',
+                      height: 500.0,
+                    ),
+                  ),
+                  
+                  SizedBox(height: sizeBox),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(50.0),
+                    child: Image.asset(
+                      'assets/imges/prang2.jpg',
+                      height: 480.0,
+                    ),
+                  ),
+                  
+                   SizedBox(height: sizeBox),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(50.0),
+                    child: Image.asset(
+                      'assets/imges/ton1.jpg',
+                      height: 480.0,
+                    ),
+                  ),
+                  
+                   SizedBox(height: sizeBox),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(50.0),
+                    child: Image.asset(
+                      'assets/imges/ton2.jpg',
+                      height: 480.0,
+                    ),
+                  ),
+                   SizedBox(height: sizeBox),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(50.0),
+                    child: Image.asset(
+                      'assets/imges/viewshowkauy.jpg',
+                      height: 480.0,
+                    ),
+                  ),
+                   SizedBox(height: sizeBox),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(50.0),
+                    child: Image.asset(
+                      'assets/imges/viewshowkauyaroay.jpg',
+                      height: 480.0,
+                    ),
+                  ),
+                  
+                  SizedBox(height: sizeBox),
+                  ListTile(
+                    
+                    title: Align(
+                      
+                      alignment: Alignment(-1, 0),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
